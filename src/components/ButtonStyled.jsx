244a753ffc39dc066677c7d5a241b45bb10e0ed7
Denison Kolling <denisonkolling@gmail.com>
2023-08-02 12:28:16 -0300

    ex 04 adiciona componente button

---
+++ src/components/ButtonStyled.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ButtonStyled = ({children}) => {
+  return (
+    <button className='btn btn-outline-dark m-2'>{children}</button>
+  )
+}
+
+export default ButtonStyled
